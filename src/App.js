7d5884e8e13e93b446fce919f868b5656e61a13e
fadhili-ed <fadhilied00@gmail.com>
2020-05-17 12:26:26 +0300

    create a react front end app, this is the initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
